7ea4bb64ff6f3bf5829e6ed829896e642536281a
jeganumapathy <thalajegan@gmail.com>
2014-04-09 10:14:34 +0530

    added magma project file

---
+++ Magnetized-desktop/src/com/f5/Magnetized/Main.java
@@ -0,0 +1,16 @@
+package com.f5.Magnetized;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Magnetized";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new MagnetizedGame(), cfg);
+	}
+}
